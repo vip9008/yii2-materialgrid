3337d83f0493b385f1a8ae201af86ac6e6db16bd
Hussain Almoamen <ave9008@gmail.com>
2016-11-06 15:46:33 +0300

    Revert "initial extension commit."
    This reverts commit 20409fdf028eeafac06f27af8734a81e7aab24db.

---
+++ AutoloadExample.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace vip9008\materialgrid;
+
+/**
+ * This is just an example.
+ */
+class AutoloadExample extends \yii\base\Widget
+{
+    public function run()
+    {
+        return "Hello!";
+    }
+}
