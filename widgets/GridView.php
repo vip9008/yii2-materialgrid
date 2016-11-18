8b45b0b840e949284d16af0d7de7d9910e321159
Hussain Almoamen <ave9008@gmail.com>
2016-11-18 21:30:06 +0300

    Material Grid GridView class

--- widgets/GridView.php
+++ widgets/GridView.php
@@ -5,6 +5,7 @@ namespace vip9008\materialgrid\widgets;
 use Yii;
 use yii\helpers\ArrayHelper;
 use yii\helpers\Html;
+use vip9008\materialgrid\widgets\LinkPager;
 use yii\grid\GridView as BaseGridView;
 
 class GridView extends BaseGridView
@@ -14,7 +15,7 @@ class GridView extends BaseGridView
     public $layout = "{items}\n{pager}\n{summary}";
     public $summaryContentOptions = ['class' => 'summary-right'];
     public $dataColumnClass = 'vip9008\materialgrid\widgets\DataColumn';
-
+    public $caption = 'something';
 
     public function renderSection($name)
     {
@@ -26,12 +27,58 @@ class GridView extends BaseGridView
             case '{items}':
                 return $this->renderItems();
             case '{pager}':
-                return $this->renderPager();
+                // return $this->renderPager();
+                return '';
             case '{sorter}':
                 return $this->renderSorter();
             default:
                 return false;
         }
+    }
+
+    public function renderCaption()
+    {
+        if (!empty($this->caption)) {
+            Html::addCssClass($this->captionOptions, 'table-header');
+            return Html::tag('div', $this->caption, $this->captionOptions);
+        } else {
+            return false;
+        }
+    }
+
+    public function renderItems()
+    {
+        $caption = $this->renderCaption();
+        $columnGroup = $this->renderColumnGroup();
+        $tableHeader = $this->showHeader ? $this->renderTableHeader() : false;
+        $tableBody = $this->renderTableBody();
+        $tableFooter = $this->showFooter ? $this->renderTableFooter() : false;
+        $content = array_filter([
+            $columnGroup,
+            $tableHeader,
+            $tableFooter,
+            $tableBody,
+        ]);
+
+        $content = Html::tag('table', implode("\n", $content), $this->tableOptions);
+
+        return $caption . "\n" . Html::tag('div', $content, ['class' => 'table-container']);
+    }
+
+    public function renderPager()
+    {
+        $pagination = $this->dataProvider->getPagination();
+        if ($pagination === false || $this->dataProvider->getCount() <= 0) {
+            return '';
+        }
+        /* @var $class LinkPager */
+        $pager = $this->pager;
+        $class = ArrayHelper::remove($pager, 'class', LinkPager::className());
+        $pager['pagination'] = $pagination;
+        $pager['view'] = $this->getView();
+
+        $pager['renderPageNumbers'] = false;
+        return $class::widget($pager);
     }
 
     public function renderSummary()
@@ -40,6 +87,9 @@ class GridView extends BaseGridView
         if ($count <= 0) {
             return '';
         }
+
+        $paginationContent = $this->renderPager();
+
         $summaryOptions = $this->summaryOptions;
         $tag = ArrayHelper::remove($summaryOptions, 'tag', 'div');
         if (($pagination = $this->dataProvider->getPagination()) !== false) {
@@ -53,7 +103,7 @@ class GridView extends BaseGridView
             $pageCount = $pagination->pageCount;
             if (($summaryContent = $this->summary) === null) {
                 return Html::tag($tag, 
-                    Html::tag('div', Yii::t('yii', '{begin, number}-{end, number} of {totalCount, number}', [
+                    Html::tag('div', Yii::t('yii', '{begin, number}-{end, number} of {totalCount, number}' . $paginationContent, [
                         'begin' => $begin,
                         'end' => $end,
                         'totalCount' => $totalCount,
@@ -65,14 +115,14 @@ class GridView extends BaseGridView
             $end = $totalCount = $count;
             if (($summaryContent = $this->summary) === null) {
                 return Html::tag($tag, 
-                    Html::tag('div', Yii::t('yii', '{count, number}', [
+                    Html::tag('div', Yii::t('yii', '{count, number}' . $paginationContent, [
                         'count' => $count,
                     ]), $this->summaryContentOptions)
                 , $summaryOptions);
             }
         }
 
-        return Yii::$app->getI18n()->format($summaryContent, [
+        return Yii::$app->getI18n()->format($summaryContent . $paginationContent, [
             'begin' => $begin,
             'end' => $end,
             'count' => $count,
