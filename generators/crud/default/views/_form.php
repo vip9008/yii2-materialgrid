aa42fe47cf6ab1dc8744e0f182cb0624ca6051eb
Hussain Almoamen <almoamen.m.h@gmail.com>
2018-04-16 13:28:15 +0300

    create CRUD generator template

---
+++ generators/crud/default/views/_form.php
@@ -0,0 +1,59 @@
+<?php
+
+use yii\helpers\Inflector;
+use yii\helpers\StringHelper;
+
+/* @var $this yii\web\View */
+/* @var $generator yii\gii\generators\crud\Generator */
+
+/* @var $model \yii\db\ActiveRecord */
+$model = new $generator->modelClass();
+$safeAttributes = $model->safeAttributes();
+if (empty($safeAttributes)) {
+    $safeAttributes = $model->attributes();
+}
+
+echo "<?php\n";
+?>
+
+use yii\helpers\ArrayHelper;
+use vip9008\materialgrid\helpers\Html;
+use vip9008\materialgrid\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model <?= ltrim($generator->modelClass, '\\') ?> */
+/* @var $form yii\widgets\ActiveForm */
+$primaryColor = ArrayHelper::getValue(Yii::$app->params, 'primaryColor', 'indigo');
+$accentColor = ArrayHelper::getValue(Yii::$app->params, 'accentColor', 'blue');
+?>
+
+<div class="space"></div>
+<div class="space"></div>
+
+<div class="container">
+    <div class="row">
+        <div class="col medium-12">
+            <section class="chapter <?= Inflector::camel2id(StringHelper::basename($generator->modelClass)) ?>-form">
+                <h1 class="chapter-title <?= '<?= $primaryColor ?>' ?>"><?= '<?= $this->title ?>' ?></h1>
+
+                <div class="expansion-panel">
+                <?= '<?php $form = ActiveForm::begin(["themeColor" => $accentColor]); ?>' ?>
+                <div class="panel active">
+                    <div class="label"><?= '<?= $this->title ?>' ?></div>
+                    <div class="content"></div>
+                    <div class="expansion-content">
+                        <?php foreach ($generator->getColumnNames() as $attribute) {
+                            if (in_array($attribute, $safeAttributes)) {
+                                echo "<?= " . $generator->generateActiveField($attribute) . " ?>\n\n";
+                            }
+                        } ?>
+                    </div>
+                    <div class="expansion-controls">
+                        <?= "<?= " ?>Html::submitButton(<?= $generator->generateString('Save') ?>, ['class' => "btn $accentColor"]) ?>
+                    </div>
+                </div>
+                <?= "<?php " ?>ActiveForm::end(); ?>
+            </section>
+        </div>
+    </div>
+</div>
---
+++ generators/crud/default/views/view.php
@@ -0,0 +1,68 @@
+<?php
+
+use yii\helpers\Inflector;
+use yii\helpers\StringHelper;
+
+/* @var $this yii\web\View */
+/* @var $generator yii\gii\generators\crud\Generator */
+
+$urlParams = $generator->generateUrlParams();
+
+echo "<?php\n";
+?>
+
+use yii\helpers\ArrayHelper;
+use vip9008\materialgrid\helpers\Html;
+use vip9008\materialgrid\widgets\DetailView;
+
+/* @var $this yii\web\View */
+/* @var $model <?= ltrim($generator->modelClass, '\\') ?> */
+
+$this->title = $model-><?= $generator->getNameAttribute() ?>;
+$this->params['breadcrumbs'][] = ['label' => <?= $generator->generateString(Inflector::pluralize(Inflector::camel2words(StringHelper::basename($generator->modelClass)))) ?>, 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+
+$primaryColor = ArrayHelper::getValue(Yii::$app->params, 'primaryColor', 'indigo');
+$accentColor = ArrayHelper::getValue(Yii::$app->params, 'accentColor', 'blue');
+?>
+<section class="chapter <?= Inflector::camel2id(StringHelper::basename($generator->modelClass)) ?>-view">
+    <div class="container">
+        <div class="row">
+            <div class="col large-12">
+                <h1 class="chapter-title <?= "<?= " ?>$primaryColor ?>"><?= "<?= " ?>Html::encode($this->title) ?></h1>
+
+                <?= "<?= " ?>DetailView::widget([
+                    'model' => $model,
+                    'attributes' => [
+                        <?php
+                        if (($tableSchema = $generator->getTableSchema()) === false) {
+                            foreach ($generator->getColumnNames() as $name) {
+                                echo "'" . $name . "',\n";
+                            }
+                        } else {
+                            foreach ($generator->getTableSchema()->columns as $column) {
+                                $format = $generator->generateColumnFormat($column);
+                                echo "'" . $column->name . ($format === 'text' ? "" : ":" . $format) . "',\n";
+                            }
+                        }
+                        ?>
+                    ],
+                ]) ?>
+
+                <div class="btn-group">
+                    <?= "<?= " ?>Html::a(<?= $generator->generateString('Delete') ?>, ['delete', <?= $urlParams ?>], [
+                        'class' => "btn raised color $accentColor",
+                        'data' => [
+                            'confirm' => <?= $generator->generateString('Are you sure you want to delete this item?') ?>,
+                            'method' => 'post',
+                        ],
+                    ]) ?>
+                    <?= "<?= " ?>Html::a(<?= $generator->generateString('Update') ?>, ['update', <?= $urlParams ?>], [
+                        'class' => "btn raised color $primaryColor",
+                        ]) ?>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</section>
